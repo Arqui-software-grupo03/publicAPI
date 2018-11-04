0e2df6b0d3e9d8778f97800f29ec2fd51e980333
iguridi <ignacio@Ignacios-MacBook-Pro.local>
2018-11-04 17:06:21 -0300

    union con api topics casi lista

--- app/controllers/topics.js
+++ app/controllers/topics.js
@@ -1,54 +1,55 @@
 import axios from 'axios';
 
-const dir = 'http://loaclhost:8000/topics';
+const dir = 'http://apptopics:8080/topics';
 
 
 class TopicsControllers {
     /* eslint-disable no-param-reassign */
 
     async create(ctx) {
-        const ans = await axios.post(`${dir}`, ctx.request.body);
-        ctx.body = ans;
+        const ans = await axios.post(`${dir}/`, ctx.request.body);
+        ctx.body = ans.data;
     }
-
-    async delete(ctx) {
+    
+    async delete(ctx) { // not working
         const ans = await axios.delete(`${dir}/${ctx.params.id}`);
-        ctx.body = ans;
+        ctx.body = ans.data;
     }
-
+    
     async show(ctx) {
         const ans = await axios.get(`${dir}/${ctx.params.id}`);
-        ctx.body = ans;
+        ctx.body = ans.data;
     }
-
+    
     async all(ctx) {
         const ans = await axios.get(`${dir}`);
-        ctx.body = ans;
+        ctx.body = ans.data;
     }
     
     async categorize(ctx) {
-        const ans = await axios.post(`${dir}/${ctx.params.id}/post_ids`, ctx.request.body);
-        ctx.body = ans;
+        console.log('ahahah', ans);
+        const ans = await axios.post(`${dir}/${ctx.params.id}/post_ids/`, ctx.request.body);
+        ctx.body = ans.data;
     }
 
-    async deleteCategorization(ctx) {
+    async deleteCategorization(ctx) { // tampoco. probklema de la apitopics?
         const ans = await axios.delete(`${dir}/${ctx.params.id}/post_ids/${ctx.params.postId}`);
-        ctx.body = ans;
+        ctx.body = ans.data;
     }
 
     async listPosts(ctx) {
         const ans = await axios.get(`${dir}/${ctx.params.id}/post_ids`);
-        ctx.body = ans;
+        ctx.body = ans.data;
     }
 
     async subscribe(ctx) {
-        const ans = await axios.post(`${dir}/${ctx.params.id}/subscribers`, ctx.request.body);
-        ctx.body = ans;
+        const ans = await axios.post(`${dir}/${ctx.params.id}/subscribers/`, ctx.request.body);
+        ctx.body = ans.data;
     }
 
     async unsubscribe(ctx) {
         const ans = await axios.delete(`${dir}/${ctx.params.id}/subscribers/${ctx.params.userId}`);
-        ctx.body = ans;
+        ctx.body = ans.data;
     }
 
 
