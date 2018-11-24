4429fb71752c3502deab939813c3b6eea6bb2ac7
Stephanie Chau <schau@uc.cl>
2018-11-24 09:27:58 -0300

    Unified url from config to posts

--- app/config.js
+++ app/config.js
@@ -3,5 +3,5 @@
 export const port = 3000;
 export const connexionString = 'mongodb://mongo/test';
 export const baseApi = 'api';
-export const dirTopics = 'http://topics:8080/topics'; // 'http://172.28.0.4:8080/topics'; //
-export const dirPosts = 'http://posts:8100/posts'; // 'http://172.28.0.3:8100/posts'; //
+export const dirTopics = 'http://topics:8080/topics'; // 'http://172.28.0.4:8080/topics';
+export const dirPosts =  'http://posts:8100/posts'; // 'http://172.28.0.3:8100/posts';
